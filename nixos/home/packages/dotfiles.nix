c929ba2dc018619e6ecbbdd81959919900cd1a33
= <=>
2024-09-12 16:34:15 -0700

    [system] debug home-manager lag
    Home-manager spends a lot of time starting up, some research suggests it
    could be my dotfiles folders. This attempts to debunk that by symlinking
    folders directly

--- nixos/home/packages/dotfiles.nix
+++ nixos/home/packages/dotfiles.nix
@@ -1,50 +1,21 @@
-{ ... }:
+{ config, ... }:
 
 let
   dotfiles = builtins.toString ../../../dotfiles;
 in
 {
   xdg.configFile = {
-    "gtk-3.0" = {
-      source = "${dotfiles}/gtk/gtk-3.0";
-      recursive = true;
-      force = true;
-    };
-    "gtk-4.0" = {
-      source = "${dotfiles}/gtk/gtk-4.0";
-      recursive = true;
-      force = true;
-    };
-    "qt5ct" = {
-      source = "${dotfiles}/gtk/qt5ct";
-      recursive = true;
-      force = true;
-    };
-    "qt6ct" = {
-      source = "${dotfiles}/gtk/qt6ct";
-      recursive = true;
-      force = true;
-    };
-    "rofi" = {
-      source = "${dotfiles}/rofi";
-      recursive = true;
-      force = true;
-    };
+    "gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/gtk-3.0";
+    "gtk-4.0".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/gtk-4.0";
+    "qt5ct".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/qt5ct";
+    "qt6ct".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/qt6ct";
+    "rofi".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/rofi";
   };
   home.file = {
-    ".themes" = {
-      source = "${dotfiles}/gtk/themes";
-      recursive = true;
-      force = true;
-    };
-    ".icons" = {
-      source = "${dotfiles}/gtk/icons";
-      recursive = true;
-      force = true;
-    };
+    ".themes".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/themes";
+    ".icons".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/icons";
     "Pictures/wallpapers" = {
       source = "${dotfiles}/wallpapers";
-      recursive = true;
       force = true;
     };
   };
