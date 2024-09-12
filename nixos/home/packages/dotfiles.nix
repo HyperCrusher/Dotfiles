3adb9a3162d6bc9a3bd7c37eb6bcea634b94bf50
= <=>
2024-09-12 10:59:20 -0700

    Reference new gtk location

--- nixos/home/packages/dotfiles.nix
+++ nixos/home/packages/dotfiles.nix
@@ -6,27 +6,37 @@ in
 {
   xdg.configFile = {
     "gtk-3.0" = {
-      source = "${dotfiles}/gtk/.config/gtk-3.0";
+      source = "${dotfiles}/gtk/gtk-3.0";
       recursive = true;
       force = true;
     };
     "gtk-4.0" = {
-      source = "${dotfiles}/gtk/.config/gtk-4.0";
+      source = "${dotfiles}/gtk/gtk-4.0";
       recursive = true;
       force = true;
     };
     "qt5ct" = {
-      source = "${dotfiles}/gtk/.config/qt5ct";
+      source = "${dotfiles}/gtk/qt5ct";
       recursive = true;
       force = true;
     };
     "qt6ct" = {
-      source = "${dotfiles}/gtk/.config/qt6ct";
+      source = "${dotfiles}/gtk/qt6ct";
       recursive = true;
       force = true;
     };
   };
   home.file = {
+    ".themes" = {
+      source = "${dotfiles}/gtk/themes";
+      recursive = true;
+      force = true;
+    };
+    ".icons" = {
+      source = "${dotfiles}/gtk/icons";
+      recursive = true;
+      force = true;
+    };
     "Pictures/wallpapers" = {
       source = "${dotfiles}/wallpapers";
       recursive = true;
