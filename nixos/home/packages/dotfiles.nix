087b1a4f9b93738fdce8ab62be1e7cc1bc2e0523
= <=>
2024-09-12 05:12:20 -0700

    Debug issues with dotfiles.nix
    Try to cleanup dotfiles.nix so that home-manager can use it, home
    manager seems to be struggling to switch with old config

--- nixos/home/packages/dotfiles.nix
+++ nixos/home/packages/dotfiles.nix
@@ -1,11 +1,31 @@
 { ... }:
+
+let
+  dotfiles = builtins.toString ../../../dotfiles;
+in
 {
+  xdg.configFile = {
+    "gtk-3.0" = {
+      source = "${dotfiles}/gtk/.config/gtk-3.0";
+      recursive = true;
+    };
+    "gtk-4.0" = {
+      source = "${dotfiles}/gtk/.config/gtk-4.0";
+      recursive = true;
+    };
+    "qt5ct" = {
+      source = "${dotfiles}/gtk/.config/qt5ct";
+      recursive = true;
+    };
+    "qt6ct" = {
+      source = "${dotfiles}/gtk/.config/qt6ct";
+      recursive = true;
+    };
+  };
   home.file = {
-    ".config/gtk-3.0".source = ../../../dotfiles/gtk/.config/gtk-3.0;
-    ".config/gtk-4.0".source = ../../../dotfiles/gtk/.config/gtk-4.0;
-    ".config/qt5ct".source = ../../../dotfiles/gtk/.config/qt5ct;
-    ".config/qt6ct".source = ../../../dotfiles/gtk/.config/qt6ct;
-    # Wallpapers
-    "Pictures/wallpapers".source = ../../../dotfiles/wallpapers;
+    "Pictures/wallpapers" = {
+      source = "${dotfiles}/wallpapers";
+      recursive = true;
+    };
   };
 }
