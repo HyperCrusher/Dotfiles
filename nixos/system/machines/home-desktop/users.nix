4ff1c84e1882c9c7006276b24f66638550f75c20
= <=>
2024-09-12 14:54:39 -0700

    [nixos] Set zsh as default shell for home-desktop

--- nixos/system/machines/home-desktop/users.nix
+++ nixos/system/machines/home-desktop/users.nix
@@ -1,34 +1,37 @@
-{ user, ... }:
+{ user, pkgs, ... }:
 {
-  users.users = {
-    ${user.name_lower} = {
-      isNormalUser = true;
-      home = "/home/${user.name_lower}";
-      description = "${user.name}";
-      initialPassword = "changeme";
-      extraGroups = [
-        "flatpak"
-        "disk"
-        "qemu"
-        "kvm"
-        "docker"
-        "wireshark"
-        "libvirtd"
-        "networkmanager"
-        "wheel"
-        "audio"
-        "video"
-      ];
-    };
-    work = {
-      isNormalUser = true;
-      home = "/home/work";
-      description = "Work";
-      initialPassword = "changeme";
-      extraGroups = [
-        "flatpak"
-        "wheel"
-      ];
+  users = {
+    defaultShell = pkgs.zsh;
+    users = {
+      ${user.name_lower} = {
+        isNormalUser = true;
+        home = "/home/${user.name_lower}";
+        description = "${user.name}";
+        initialPassword = "changeme";
+        extraGroups = [
+          "flatpak"
+          "disk"
+          "qemu"
+          "kvm"
+          "docker"
+          "wireshark"
+          "libvirtd"
+          "networkmanager"
+          "wheel"
+          "audio"
+          "video"
+        ];
+      };
+      work = {
+        isNormalUser = true;
+        home = "/home/work";
+        description = "Work";
+        initialPassword = "changeme";
+        extraGroups = [
+          "flatpak"
+          "wheel"
+        ];
+      };
     };
   };
 }
