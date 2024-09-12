14a3f2bf0702b50086bbd1fa3ea8bcdbd2f17f31
= <=>
2024-09-12 05:30:32 -0700

    Add force flag
    I will always want my dotfiles to take over, even if another application
    has attempted to overwrite my files

--- nixos/home/packages/dotfiles.nix
+++ nixos/home/packages/dotfiles.nix
@@ -8,24 +8,29 @@ in
     "gtk-3.0" = {
       source = "${dotfiles}/gtk/.config/gtk-3.0";
       recursive = true;
+      force = true;
     };
     "gtk-4.0" = {
       source = "${dotfiles}/gtk/.config/gtk-4.0";
       recursive = true;
+      force = true;
     };
     "qt5ct" = {
       source = "${dotfiles}/gtk/.config/qt5ct";
       recursive = true;
+      force = true;
     };
     "qt6ct" = {
       source = "${dotfiles}/gtk/.config/qt6ct";
       recursive = true;
+      force = true;
     };
   };
   home.file = {
     "Pictures/wallpapers" = {
       source = "${dotfiles}/wallpapers";
       recursive = true;
+      force = true;
     };
   };
 }
