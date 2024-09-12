5b489530412654b6a4a027e6be51765254f4b71d
= <=>
2024-09-12 16:39:05 -0700

    [system] re-add force=true
    Force the symlinks to take hold, even if other files exist in the space

--- nixos/home/packages/dotfiles.nix
+++ nixos/home/packages/dotfiles.nix
@@ -5,15 +5,36 @@ let
 in
 {
   xdg.configFile = {
-    "gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/gtk-3.0";
-    "gtk-4.0".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/gtk-4.0";
-    "qt5ct".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/qt5ct";
-    "qt6ct".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/qt6ct";
-    "rofi".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/rofi";
+    "gtk-3.0" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/gtk-3.0";
+      force = true;
+    };
+    "gtk-4.0" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/gtk-4.0";
+      force = true;
+    };
+    "qt5ct" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/qt5ct";
+      force = true;
+    };
+    "qt6ct" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/qt6ct";
+      force = true;
+    };
+    "rofi" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/rofi";
+      force = true;
+    };
   };
   home.file = {
-    ".themes".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/themes";
-    ".icons".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/icons";
+    ".themes" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/themes";
+      force = true;
+    };
+    ".icons" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/gtk/icons";
+      force = true;
+    };
     "Pictures/wallpapers" = {
       source = "${dotfiles}/wallpapers";
       force = true;
