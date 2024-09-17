27b180dfdc6130b2b80e98be1fa9e8d6a7eca96b
Hyper <hypercrusher@proton.me>
2024-09-16 19:47:39 -0700

    [nvim] Added fold keybinds
    I almost never use folds in neovim, and I thinks its probably keybind
    related, so Im going to make these keybinds to try to make myself use
    them more

--- dotfiles/nvim/lua/keymaps/visual.lua
+++ dotfiles/nvim/lua/keymaps/visual.lua
@@ -5,6 +5,10 @@ setKeymap("v", "/", '"fy/\\V<C-R>f<CR>N')
 setKeymap("v", ">", ">gv")
 setKeymap("v", "<", "<gv")
 
+-- Fold Control
+setKeymap("v", "<leader>f", "zf")
+setKeymap("n", "<leader>f", "zd")
+
 -- Indent with Tab key
 setKeymap("v", "<TAB>", ">gv")
 setKeymap("v", "<S-TAB>", "<gv")
